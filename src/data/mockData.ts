3fc2010204bbc2aedf775b60c04cf299e571cc12
xynoan <ncmorva@gmail.com>
2026-03-08 19:25:04 +0800

    remove more unnecessary files

--- src/data/mockData.ts
+++
@@ -1,8 +0,0 @@
-import { Vehicle, Camera, Violation } from '@/types/parking';
-
-// Empty arrays - data will be added through the UI
-export const mockVehicles: Vehicle[] = [];
-
-export const mockCameras: Camera[] = [];
-
-export const mockViolations: Violation[] = [];
